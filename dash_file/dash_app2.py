d29d31846fc98708c2e88b9f82dc1def9ccd8578
spread11 <yyhhnnjj@gmail.com>
2023-12-15 10:43:51 +0800

    modify

--- dash_file/dash_app2.py
+++ dash_file/dash_app2.py
@@ -6,7 +6,6 @@ import pandas as pd
 dash2 = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 dash2.title = "BaHaMutAnime"
 df = pd.read_csv('BaHaMut_9.csv')
-df1 = df.reset_index()
 dash2.layout = html.Div(
     [
 
@@ -21,12 +20,14 @@ dash2.layout = html.Div(
             html.Div([
                 dash_table.DataTable(
                     id='main_table',
-                    data=df1.to_dict('records'),
+                    data=df.to_dict('records'),
                     columns=[{'id': column, 'name': column}
-                             for column in df1.columns],
+                             for column in df.columns],
                     sort_action='native',  # 启用原生排序功能
                     sort_mode='multi',
-                    style_cell={'whiteSpace':'normal','textAlign':'left'},
+                    style_cell={'whiteSpace': 'normal', 'textAlign': 'center'},
+                    style_cell_conditional=[
+                        {'if': {'column_id': '動畫名'}, 'width': '190px'}, {'if': {'column_id': '年份'}, 'width': '75px'}, {'if': {'column_id': '月份'}, 'width': '75px'}, {'if': {'column_id': '集數'}, 'width': '75px'}, {'if': {'column_id': '星級'}, 'width': '75px'}, {'if': {'column_id': '評分人數'}, 'width': '50px'}, {'if': {'column_id': '導演監督'}, 'width': '80px'}, {'if': {'column_id': '製作廠商'}, 'width': '85px'}, {'if': {'column_id': '作品分類1'}, 'width': '50px'}, {'if': {'column_id': '作品分類2'}, 'width': '50px'}, {'if': {'column_id': '作品分類3'}, 'width': '50px'}, {'if': {'column_id': '作品分類4'}, 'width': '50px'}, {'if': {'column_id': '作品分類5'}, 'width': '50px'}, {'if': {'column_id': '作品分類6'}, 'width': '50px'}, {'if': {'column_id': '原作載體'}, 'width': '50px'}, {'if': {'column_id': '新續作'}, 'width': '65px'}, {'if': {'column_id': '平均觀看數(萬)'}, 'width': '75px'}, {'if': {'column_id': '總觀看數(萬)'}, 'width': '75px'}],
                     page_size=30,
                     # style_table={'height': '800px', 'overflowY': 'auto'},
                     fixed_rows={'headers': True},
@@ -57,9 +58,9 @@ def selectedRow(selected_rows: list[int]):
     # 取得一個站點,series
     if len(selected_rows) != 0:
         print("執行")
-        oneSite: pd.DataFrame = df1.iloc[[selected_rows[0]]]
+        oneSite: pd.DataFrame = df.iloc[[selected_rows[0]]]
         oneTable: dash_table.DataTable = dash_table.DataTable(
-            oneSite.to_dict('records'), [{"name": i, "id": i} for i in oneSite.columns])
+            oneSite.to_dict('records'), [{"name": i, "id": i} for i in oneSite.columns], style_cell={'whiteSpace': 'normal', 'textAlign': 'center'})
         return [oneTable]
 
     return None
