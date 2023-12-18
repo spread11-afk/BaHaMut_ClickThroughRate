fcf30998456b8847786d3b585045ab65b75395b1
spread11 <yyhhnnjj@gmail.com>
2023-12-18 17:11:30 +0800

    modify

--- dash_file/dash_app2.py
+++ dash_file/dash_app2.py
@@ -18,6 +18,7 @@ def b64_image(image_filename):
 dash2 = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 dash2.title = "BaHaMutAnime"
 df = pd.read_csv('BaHaMut_9.csv')
+df['動畫名'] = df['動畫名']
 dash2.layout = html.Div(
     [
 
@@ -27,6 +28,19 @@ dash2.layout = html.Div(
             ], className="title")
         ],
             className="title-row",
+            style={"paddingTop": '2rem'}),
+        html.Div([
+            html.Div([
+                html.Div([
+                    dbc.Input(id='input_value',
+                              placeholder="請輸入動畫名稱", type="text"),
+                ])
+            ], className="col"),
+            html.Div([
+                html.Button('確定', id='submit-val', className="btn btn-primary")
+            ], className="col")
+        ],
+            className="row row-cols-auto align-items-end",
             style={"paddingTop": '2rem'}),
         html.Div([
             html.Div([
@@ -53,9 +67,7 @@ dash2.layout = html.Div(
             style={"paddingTop": '2rem'}),
         html.Div([
             html.Div(className="showselect",
-                     id='showMessage'),
-            html.Img(src=(
-                './assets/images/attack.png'))
+                     id='showMessage')
         ],
             className="maincontainer",
             style={"paddingTop": '2rem'}),
@@ -84,15 +96,70 @@ dash2.layout = html.Div(
 )
 def selectedRow(selected_rows):
     if len(selected_rows) != 0:
-        print("執行")
-        # oneSite: pd.DataFrame = df.iloc[[selected_rows[0]]],
+        print(df.iloc[selected_rows[0]][0])
+        oneSite: pd.DataFrame = df.iloc[[selected_rows[0]]],
+        oneSite = oneSite[0]
         # oneTable: dash_table.DataTable = dash_table.DataTable(
-            # oneSite.to_dict('records'), [{"name": i, "id": i} for i in oneSite.columns], style_cell={'whiteSpace': 'normal', 'textAlign': 'center'})
-        info = dbc.ModalHeader(dbc.ModalTitle("關於我轉生變成史萊姆這檔事 第二季"), class_name='infotitle'), html.Div([html.Img(src='assets/suraimu.png'),
-                                                                                                       html.P('三上悟過著不起眼的人生，在隨機殺人魔肆虐下結束了三十七年生涯…… 看似如此。當他甦醒時，不僅眼睛看不見，就連耳朵也聽不到…… 面對一連串突發狀況，他意識到自己投胎轉世成「史萊姆」！儘管變成最弱魔物讓他頗有怨言，三上悟還是決定要快樂地過史萊姆生活，沒想到卻碰上天災級魔物「暴風龍維爾德拉」，命運就此出現巨大轉折──維爾德拉將他命名為「利姆路」，正要展開史萊姆式的異世界新生活時，卻被捲入哥布靈對牙狼族的紛爭之中，最後還莫名其妙當上魔物大王…… 能奪取對手能力的「捕食者」以及精通世界真理的「大賢者」，有這兩項特殊技能當武器，最強的史萊姆傳說正式展開！ STAFF 原作：川上泰樹、伏瀨、みっ')], className='info')
+        #     oneSite.to_dict('records'), [{"name": i, "id": i} for i in oneSite.columns], style_cell={'whiteSpace': 'normal', 'textAlign': 'center'})
+        # for i in oneSite:
+        #     print(i)
+        # print(type(oneSite))
+        # print(oneSite)
+        # print(df.iloc[active_cell["row"], df.columns.get_loc(active_cell["column_id"])])
+        if df.iloc[selected_rows[0]][0] == '叫我對大哥 (TV版)':
+            # info = dbc.ModalHeader(dbc.ModalTitle(df.iloc[selected_rows[0]][0]), class_name='infotitle'), html.Div([html.Img(src='assets/suraimu.png'),html.Ul([html.Li(df.iloc[selected_rows[0]][6]),html.Li(df.iloc[selected_rows[0]][7])]),
+            #                                                                                                html.P('三上悟過著不起眼的人生，在隨機殺人魔肆虐下結束了三十七年生涯…… 看似如此。當他甦醒時，不僅眼睛看不見，就連耳朵也聽不到…… 面對一連串突發狀況，他意識到自己投胎轉世成「史萊姆」！儘管變成最弱魔物讓他頗有怨言，三上悟還是決定要快樂地過史萊姆生活，沒想到卻碰上天災級魔物「暴風龍維爾德拉」，命運就此出現巨大轉折──維爾德拉將他命名為「利姆路」，正要展開史萊姆式的異世界新生活時，卻被捲入哥布靈對牙狼族的紛爭之中，最後還莫名其妙當上魔物大王…… 能奪取對手能力的「捕食者」以及精通世界真理的「大賢者」，有這兩項特殊技能當武器，最強的史萊姆傳說正式展開！ STAFF 原作：川上泰樹、伏瀨、みっ')], className='info')
+            info = html.Div([html.Div(html.A([html.Img(src='assets/suraimu_waifu2x_art_noise1_scale.png')], href="https://ani.gamer.com.tw/animeVideo.php?sn=20530", target="_blank"), className='infoimage'), html.Div([html.H1([df.iloc[selected_rows[0]][0]], style={'color': 'rgba(255, 208, 0, 0.89'}), html.H3([f'監督：{df.iloc[selected_rows[0]][6]}'], style={'color': 'rgba(0, 238, 255, 0.842', 'marginTop': '25px'}), html.H3([f'製作公司：{df.iloc[selected_rows[0]][7]}'], style={'color': 'rgba(0, 238, 255, 0.842'}),
+                             html.H3(['三上悟過著不起眼的人生，在隨機殺人魔肆虐下結束了三十七年生涯…… 看似如此。當他甦醒時，不僅眼睛看不見，就連耳朵也聽不到…… 面對一連串突發狀況，他意識到自己投胎轉世成「史萊姆」！儘管變成最弱魔物讓他頗有怨言，三上悟還是決定要快樂地過史萊姆生活，沒想到卻碰上天災級魔物「暴風龍維爾德拉」，命運就此出現巨大轉折──維爾德拉將他命名為「利姆路」，正要展開史萊姆式的異世界新生活時，卻被捲入哥布靈對牙狼族的紛爭之中，最後還莫名其妙當上魔物大王…… 能奪取對手能力的「捕食者」以及精通世界真理的「大賢者」，有這兩項特殊技能當武器，最強的史萊姆傳說正式展開！ STAFF 原作：川上泰樹、伏瀨、みっ'], style={'marginTop': '25px', 'color': 'white'})], style={'padding': '15px'})], className='info')
+        # elif df.iloc[selected_rows[0]][0] == '咒術迴戰':
+
+        # elif df.iloc[selected_rows[0]][0] == 'SPY×FAMILY 間諜家家酒':
+
+        # elif df.iloc[selected_rows[0]][0] == '進擊的巨人 The Final Season':
+
+        # elif df.iloc[selected_rows[0]][0] == '無職轉生，到了異世界就拿出真本事':
+
+        # elif df.iloc[selected_rows[0]][0] == 'Re：從零開始的異世界生活 第二季':
+
+        # elif df.iloc[selected_rows[0]][0] == '86－不存在的戰區－':
+
+        # elif df.iloc[selected_rows[0]][0] == '鬼滅之刃 遊郭篇':
+
+        # elif df.iloc[selected_rows[0]][0] == '輝夜姬想讓人告白～天才們的戀愛頭腦戰～ 第二季':
+
+        # elif df.iloc[selected_rows[0]][0] == '機動戰士鋼彈 水星的魔女 Season 2':
+
+        else:
+            info = dbc.ModalHeader(dbc.ModalTitle(
+                "尚未更新資料"), class_name='infotitle')
         return info, True
         # return [oneTable]
-    return None,False
+    return None, False
+
+
+@callback(
+    [Output('main_table', 'data'), Output('main_table', 'columns'),
+     Output('main_table', 'selected_rows')],
+    [Input('submit-val', 'n_clicks')],
+    [State('input_value', 'value')]
+)
+def clickBtn(n_clicks: None | int, inputValue: str):
+    global df
+    if n_clicks is not None:
+        # 一定先檢查有沒有按button
+        dff = df[df['動畫名']==inputValue]
+        dff['動畫名'] = dff['動畫名']
+        print("按確定")
+        return df.to_dict('records'), [{'id': column, 'name': column} for column in df.columns], []
+
+    # n_clicks is None
+    # 代表第一次啟動
+    print("第一次啟動")
+    # current_data = datasource.lastest_datetime_data()
+    df = pd.read_csv('BaHaMut_9.csv')
+    df['動畫名'] = df['動畫名']
+    print("按確定")
+    return df.to_dict('records'), [{'id': column, 'name': column} for column in df.columns], []
 
 
 selected_row = None
