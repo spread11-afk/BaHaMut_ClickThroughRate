d791c7827904e4cb2dc3794326fbafdd51e5013e
spread11 <yyhhnnjj@gmail.com>
2023-12-21 06:46:34 +0800

    a

---
+++ dash_file/homepage.py
@@ -0,0 +1,25 @@
+import dash
+from dash import Dash, html, dash_table, Input, Output, callback, dcc, State
+import pandas as pd
+import dash_bootstrap_components as dbc
+import pandas as pd
+import base64
+
+
+image_filename = './assets/suraimu.png'
+
+
+def b64_image(image_filename):
+    with open(image_filename, 'rb') as f:
+        image = f.read()
+    return 'data:image/png;base64,' + base64.b64decode(image).decode('utf-8')
+
+
+dash2 = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+dash2.title = "BaHaMutAnime"
+df = pd.read_csv('BaHaMut_9.csv')
+dash2.layout = html.Div([html.Div([html.Div([html.H1("BaHaMutAnime")], className="title")],className="title-row",style={"paddingTop": '2rem'}),html.Div([html.Div(['AnimeTrailer'],className='trailer'),html.Div([html.Video(src='assets/movies/葬送のフリーレン.mp4',autoPlay=True,muted=True,loop=True)],className='movie')],className='moviebox')],className="mycontainer")
+
+
+if __name__ == "__main__":
+    dash2.run(host='127.0.0.1', port=8050, debug=True)
